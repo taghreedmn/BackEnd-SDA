5df95343d014727077335ddf0e0f27a212a08e0b
razan4577 <razanalmohammadi@icloud>
2024-09-19 15:53:47 +0300

    Create basic CRUD operations for VadioGames

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,15 @@
+using Backend_Teamwork.src.Entity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.MapControllers();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -15,5 +18,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+
 
 app.Run();
