428781ba1fe562f151c9f39164818b9f1c8f5a56
taghreedmn <taghreed.mn18@gmail.com>
2024-09-20 16:17:42 +0300

    Create basic CRUD operations for Category

---
+++ src/Entity/Category.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sda_3_online_Backend_Teamwork.src.Entity
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? CategoryName { get; set; }
+
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,16 @@
+using sda_3_online_Backend_Teamwork.src.Entity;
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
 
+app.MapControllers();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
