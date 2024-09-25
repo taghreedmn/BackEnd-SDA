95a28403435e1ece2045cbf37b787a501aff3b40
Samar Saad <114701062+SamarSaad056@users.noreply.github.com>
2024-09-25 11:05:04 +0300

    Console&Studio Controllers

---
+++ src/Entity/GameConsole.cs
@@ -0,0 +1,10 @@
+
+namespace  FusionTech.src.Entity
+//namespace sda_3_online_Backend_Teamwork.src.Controllers
+{
+    public class GameConsole
+    {
+        public int ConsoleId { get; set; }
+        public string ConsoleName { get; set;}
+    }
+}
