9ec878146738d5b78de82389a6235e70129aa5bb
lujain41 <lujainalshehri41@gmail.com>
2024-10-01 11:03:16 +0300

    Update Supplier

--- src/Entity/Supplier.cs
+++ src/Entity/Supplier.cs
@@ -1,10 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace FusionTech.src.Entity
 {
     public class Supplier
-    {
-        public Guid SupplierId { get; set; }
-        public String SupplierName { get; set; }
-        public String SupplierContact { get; set; }
-        public String SupplierBankInfo { get; set; }
-    }
+   {
+       public Guid SupplierId { get; set; } 
+
+       [Required(ErrorMessage = "Name is required")]
+       [StringLength(100, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 100 characters")]
+       public string SupplierName { get; set; }
+
+       [Required(ErrorMessage = "Contact is required")]
+        public string SupplierContact { get; set; }
+
+       [Required(ErrorMessage = "Bank information is required")]
+        public string SupplierBankInfo { get; set; }
+
+         public string SupplierPicturePath { get; set; }
+   } 
 }
+
