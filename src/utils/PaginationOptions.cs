b52fefea5dd236b05ffdea60cd0b24a687bbde17
Kazero21 <Thinker7120@hotmail.com>
2024-10-05 20:20:42 +0300

    Fix Person Sign up email being case sensitive | Fix the starting Id

--- src/utils/PaginationOptions.cs
+++ src/utils/PaginationOptions.cs
@@ -4,7 +4,6 @@ namespace FusionTech.src.Utils
     {
         public int Limit { get; set; } = 4;
         public int Offset { get; set; } = 2;
-
         public string Search { get; set; } = string.Empty;
     }
 }
