b95c67df759cd1557972a3f43d5792a8215e9ad2
lujain41 <lujainalshehri41@gmail.com>
2024-10-13 01:00:43 +0300

    Modified supplier services method

--- src/Services/Supplier/ISupplierService.cs
+++ src/Services/Supplier/ISupplierService.cs
@@ -9,8 +9,8 @@ namespace FusionTech.src.Services.supplier
     public interface ISupplierService
     {
         Task<SupplierReadDto> CreateOneAsync(SupplierCreateDto createDto);
-        Task<List<SupplierReadDto>> GetAllAsync();
-        Task<SupplierReadDto> GetByIdAsync(Guid id);
+        Task<IEnumerable<SupplierReadDto>> GetAllAsync();
+        Task<SupplierReadDto?> GetByIdAsync(Guid id);
         Task<bool> DeleteOneAsync(Guid id);
         Task<bool> UpdateOneAsync(Guid id, SupplierUpdateDto updateDto);
     }
