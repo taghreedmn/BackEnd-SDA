733426373a7c4864104595d568f783587615cb58
taghreedmn <taghreed.mn18@gmail.com>
2024-11-13 19:20:42 +0300

    save changes

--- src/DTO/VideoGameVersionDTO.cs
+++ src/DTO/VideoGameVersionDTO.cs
@@ -19,8 +19,8 @@ namespace FusionTech.src.DTO
 
         public class VideoGameVersionUpdateDto
         {
-            public Guid VideoGameVersionId { get; set; }
-            public Guid GameConsoleId { get; set; }
+            // public Guid VideoGameVersionId { get; set; }
+            // public Guid GameConsoleId { get; set; }
             public float? Price { get; set; }
         }
 
